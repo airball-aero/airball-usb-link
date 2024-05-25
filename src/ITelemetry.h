5e60ba0242cc4ef0531e70104a36e4c45e2cd830
Ihab Awad <ihab.awad@gmail.com>
2024-05-25 11:06:47 -0700

    Changed definition of telemetry messages so that all the messages can be transmitted over CANBus, with no distinction.

--- src/ITelemetry.h
+++ src/ITelemetry.h
@@ -7,27 +7,62 @@ namespace airball {
 
 class ITelemetry {
 public:
-  static const uint16_t kMessageDomainLocal = 1;
-  static const uint16_t kMessageDomainCanBus = 2;
-  static const uint16_t kMessageDomainSync = 3;
-
-  static const uint16_t kLocalMessageIdButtonPress = 1;
-  static const uint16_t kLocalMessageIdButtonRelease = 2;
-  static const uint16_t kLocalMessageIdKnobIncrement = 3;
-  static const uint16_t kLocalMessageIdKnobDecrement = 4;
-  static const uint16_t kLocalMessageIdIdentifyLeaderFollower = 5;
-  static const uint16_t kLocalMessageIdThisNodeIsLeader = 6;
-  static const uint16_t kLocalMessageIdThisNodeIsFollower = 7;
-
+  // A Message is basically the "Good Parts" of a CANBus frame with an extended
+  // ID. Only the least-significant 29 bits of the ID are effective. We use this
+  // structure in all the messages passed around by Airball componens, so that
+  // any message may or may not easily be carried over a CANBus link, depending
+  // on the physical network topology.
 #pragma pack(push, 1)
   struct Message {
-    uint16_t domain;
-    uint16_t id;
+    uint32_t id;
     uint8_t data[8];
   };
 #pragma pack(pop)
 
-   // Receive the next Message. Returns false if no message is available.
+  // Message IDs are organized into "pages". Each page contains at most
+  // 1024 codes, each of which is at most 10 bits long. The page number is
+  // at most 19 bits long. Putting these two together gives us a 29-bit
+  // CANBus extended ID.
+  //
+  // The idea behind this separation is that each page represents the set
+  // of messages of one subsystem. As long as the subsystem reserves a
+  // page here, portions of that subsystem can independently manage their
+  // codes within that page.
+
+  // Given a full 29-bit ID, return the page number.
+  static constexpr uint32_t page(uint32_t id) {
+    return id >> 10;
+  }
+
+  // Given a full 29-bit ID, return the code within the page.
+  static constexpr uint32_t code(uint32_t id) {
+    return id & 0x3ff;
+  }
+  
+  // Given a page number and a code, return a full 29-bit ID.
+  static constexpr uint32_t id(uint32_t page, uint32_t code) {
+    return (page << 10) + code;
+  }
+
+  // Page zero is reserved.
+  static const uint32_t kPageReserved = 0;
+
+  static const uint32_t kPageAirdata = 1;  // Airdata from sensors, and commands to airdata sensors
+  static const uint32_t kPageHumanInterface = 2;  // Messages to and from human interface devices
+  static const uint32_t kPageDisplaySettings = 3;  // Messages to transfer display system settings
+
+  // Codes in kPageHumanInterface. These are kind of a special case, because they are
+  // transferred between the same entities that share the airball-usb-link Git repo. Strictly
+  // speaking, these codes should be defined in a separate "human interface" Git repo.
+  static const uint32_t kHumanInterfaceButtonPress = 1;
+  static const uint32_t kHumanInterfaceButtonRelease = 2;
+  static const uint32_t kHumanInterfaceKnobIncrement = 3;
+  static const uint32_t kHumanInterfaceKnobDecrement = 4;
+  static const uint32_t kHumanInterfaceIdentifyLeaderFollower = 5;
+  static const uint32_t kHumanInterfaceThisNodeIsLeader = 6;
+  static const uint32_t kHumanInterfaceThisNodeIsFollower = 7;
+
+  // Receive the next Message. Returns false if no message is available.
   virtual bool recv(Message* m) = 0;
 
   // Send a Message. This is broadcast to everyone on the network.
